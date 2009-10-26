a4a303b364488975d27a31e7f5fa8b41faf03562
fabiom <fabiom@2fa1dc7e-98ce-11de-bff5-93fd3790126f>
2009-10-26 03:23:42 +0000

    slides

---
+++ trunk/slides/INF2062_partial/compileBibtex.bat
@@ -0,0 +1,10 @@
+pdflatex INF2062_partial.tex
+pdflatex INF2062_partial.tex
+bibtex INF2062_partial
+bibtex INF2062_partial
+bibtex INF2062_partial
+pdflatex INF2062_partial.tex
+pdflatex INF2062_partial.tex
+pdflatex INF2062_partial.tex
+delete.bat
+@pause
