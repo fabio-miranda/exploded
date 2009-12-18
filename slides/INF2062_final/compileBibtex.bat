d93dccd236053c8d769b4976345b6c7db67a04ed
fabiom <fabiom@2fa1dc7e-98ce-11de-bff5-93fd3790126f>
2009-12-18 17:20:57 +0000

    INF2062_final

---
+++ slides/INF2062_final/compileBibtex.bat
@@ -0,0 +1,10 @@
+pdflatex INF2062_final.tex
+pdflatex INF2062_final.tex
+bibtex INF2062_final
+bibtex INF2062_final
+bibtex INF2062_final
+pdflatex INF2062_final.tex
+pdflatex INF2062_final.tex
+pdflatex INF2062_final.tex
+delete.bat
+@pause
